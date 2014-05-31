39cbb97fe6282cacf592e1fe385e93641f947c6b
Brandon Tricou <brandon.tricou@gmail.com>
2014-05-31 12:09:54 -0400

    Add ability to add overflows

--- app/models/overflow.rb
+++ app/models/overflow.rb
@@ -1,7 +1,11 @@
 class Overflow < ActiveRecord::Base
   belongs_to :agency
 
+  validates :item_type, :description, :expiration, presence: true
+  PERISHABLE = [true, false]
+  validates :perishable, inclusion: {in: PERISHABLE}
+
   def self.most_recent
   	where("created_at >= ?", 2.weeks.ago).order("created_at desc")
-  end
+  end 
 end
